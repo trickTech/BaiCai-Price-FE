43fa58bfe6c3e591e8776a7a1d52a4e29ec5fe54
echaoo <zmk181@gmail.com>
2016-10-03 16:38:27 +0800

    finish header component, begin list component

---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="title">
+    <h3>每日菜价</h3>
+    <div class="search">
+      <input placeholder="Search"/>
+      <button class="search-btn">搜索</button>
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+  .title {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background-color: green;
+    color: #fff;
+    padding: 9px 0 10px 0;
+
+    h3{
+      margin: 0 0 6px 0;
+    }
+
+    .search {
+      width: 100%;
+      padding: 0;
+
+
+
+      input {
+        width: 80%;
+        height: 30px;
+        border: none;
+        vertical-align: middle;
+      }
+
+      .search-btn {
+        width: 15%;
+        height: 30px;
+        border: none;
+        vertical-align: middle;
+      }
+    }
+
+
+  }
+</style>
+<script>
+  export default{
+    data(){
+      return {
+        msg: 'hello vue'
+      }
+    }
+  }
+</script>
---
+++ src/components/MainList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>nihao</h1>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        }
+    }
+</script>
