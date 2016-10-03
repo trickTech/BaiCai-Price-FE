d4cee509179bf31a93a3e29d519a79c4f1b8f241
echaoo <zmk181@gmail.com>
2016-10-03 17:09:40 +0800

    暂存

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -1,17 +1,44 @@
 <template>
-    <div>
-      <h1>nihao</h1>
+  <div class="main-list">
+    <div class="cell">
+      <span>菜名</span>
+      <span class="right">最高价</span>
+      <span class="right">最低价</span>
+      <span class="right">平均价</span>
     </div>
+  </div>
 </template>
-<style>
+<style lang="less" scoped>
+  .main-list {
+    margin-top: 76.333px;
 
+    .cell {
+      border-bottom: 1px solid green;
+      /*height: 40px;*/
+      font-size: 18px;
+      text-align: left;
+      padding: 16px 10px;
+
+      span {
+        margin-right: 5px;
+      }
+
+      .right {
+        float: right;
+      }
+    }
+  }
 </style>
 <script>
-    export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
-        }
+
+  import API from '../config/request'
+
+  export default{
+    data(){
+      return {
+        msg: 'hello vue'
+      }
     }
+
+  }
 </script>
---
+++ src/config/request.js
@@ -0,0 +1,11 @@
+/**
+ * Created by echao on 2016/10/3.
+ */
+
+const requestDomain = 'http://vegetable.xkcd.com.cn/';
+
+var requestConfig = {
+  vegetable: requestDomain + '/api/record/today'
+};
+
+module.exports = requestConfig;
