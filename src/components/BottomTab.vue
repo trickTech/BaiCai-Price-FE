c5b2b4f192ef681748009a0958782c306a3b1d77
echaoo <zmk181@gmail.com>
2016-10-07 16:22:04 +0800

    add BottomTab

--- src/components/BottomTab.vue
+++ src/components/BottomTab.vue
@@ -1,12 +1,51 @@
 <template>
   <div class="bottom-tab">
-    <span class="tab">筛选</span>
-    <span class="tab">历史</span>
-    <span class="tab">推荐</span>
-    <span class="tab">关于</span>
+    <span class="tab" v-on:click="changeNav(1)">筛选</span>
+    <span class="tab" v-on:click="changeNav(2)">历史</span>
+    <span class="tab" v-on:click="changeNav(3)">推荐</span>
+    <span v-if="status==0" class="tab" v-on:click="changeNav(4)">关于</span>
+    <span v-if="status==1" class="tab" v-on:click="changeNav(5)">主页</span>
   </div>
 </template>
+<script>
 
+  export default {
+    data () {
+      return {
+        status: 0
+      }
+    },
+    methods: {
+      changeNav (newNav) {
+        switch (newNav) {
+          case 1:
+                this.$router.forward('/error');
+                break;
+          case 4:
+                this.$router.replace('/about');
+                break;
+          case 5:
+                this.$router.replace('/');
+                break;
+        }
+        this.whereAmI();
+      },
+      whereAmI () {
+        switch (this.$route.path){
+          case '/':
+                this.status = 0;
+                break;
+          case '/about':
+                this.status = 1;
+                break;
+        }
+      }
+    },
+    mounted () {
+      this.whereAmI();
+    }
+  }
+</script>
 <style>
   .bottom-tab{
     position: fixed;
@@ -19,12 +58,13 @@
     float: left;
     width: 25%;
     padding: 15px;
-    background-color: white;
-    border: 1px solid green;
+    background-color: green;
+    border: 1px solid white;
     box-sizing: border-box;
-  }
+    color: white;
 
-  .tab:hover{
-    background: darkseagreen;
+    &:active{
+       background: darkseagreen;
+     }
   }
 </style>
