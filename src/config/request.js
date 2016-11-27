7767052bfeb5ec8f2a82373e2fe8a1a68c368683
sky-ADMIN <546485299@qq.com>
2016-11-27 23:36:00 +0800

    change config

--- src/config/request.js
+++ src/config/request.js
@@ -2,7 +2,7 @@
  * Created by echao on 2016/10/3.
  */
 
-const requestDomain = 'http://vegetable.xkcd.com.cn/';
+const requestDomain = 'http://115.159.184.76:8000/';
 
 var requestConfig = {
   vegetable: requestDomain + 'api/record/today',
